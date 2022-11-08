db2379cfaff44c49fdaf08b6300c7c9c929378d0
alexdonay <alexdonay@gmail.com>
2022-11-08 11:56:24 -0300

    Fila
    Signed-off-by: alexdonay <alexdonay@gmail.com>

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 # !/usr/bin/python3
-import filaprioridade2
+from filaprioridade2 import *
 from tkinter import *
+from tkinter import ttk
 
-nova = filaprioridade2.fila([{"prioridade": 3, "dado": "estudar"}])
-nova.append(1, "estudar")
-nova.append(4, "viajar")
-nova.append(2, "trabalhar")
+def preencheWindow():
+    
+    tree.column("# 1", anchor=CENTER)
+    tree.heading("# 1", text="Prioridade")
+    tree.column("# 2", anchor=CENTER)
+    tree.heading("# 2", text="Ação")
 
-class janela:
-    def __init__(self, topLevel):
-        self.fr1 = Frame(topLevel)
-        self.fr1.pack()
-        self.botao = Button(self.fr1, text='Oi!', command=self.call)
-        self.botao.pack()
-    def call(self):
-        print("oi")
-raiz = Tk()
-janela(raiz)
-raiz.mainloop()
+    for i in range(len(nova.arr)):
+        tree.insert("", "end", text=i, values=(
+            nova.arr[i]["prioridade"], nova.arr[i]["dado"]))
+
+def atualizaWindow():
+    for i in tree.get_children():
+        tree.delete(i)
+    preencheWindow()
+
+def removerFila():
+    nova.remove()
+    atualizaWindow()
+
+def incluirFila():
+    nova.append((int)(textPrioridade.get()), textDado.get())
+    atualizaWindow()
+
+nova = fila([{"prioridade": 1, "dado": "estudar"},{"prioridade": 1, "dado": "estudar"},{"prioridade": 1, "dado": "estudar"},{"prioridade": 1, "dado": "estudar"}])
+
+window = Tk()
+window.title("Estrutura de dados - Fila")
+tree = ttk.Treeview(window, column=("c1", "c2"), show='headings', height=10)
+preencheWindow()
+btnRemover = ttk.Button(text="Remover", command=removerFila)
+btnIncluir = ttk.Button(text="Incluir", command=incluirFila)
+lblPrioridade = ttk.Label(text="Prioridade")
+textPrioridade = ttk.Entry()
+lblDado = ttk.Label(text="Dado")
+textDado = ttk.Entry()
+
+
+
+tree.pack()
+btnRemover.pack()
+lblPrioridade.pack()
+textPrioridade.pack()
+lblDado.pack()
+textDado.pack()
+btnIncluir.pack()
+
+window.mainloop()
